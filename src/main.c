b468f42b91bca819fa4d19d740fc143c0f20db19
ChethanJS07 <jschethan75@gmail.com>
2026-01-19 15:13:53 +0530

    added type command

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define NUM_BUILTINS 3
+
 char *getCommand(char *input) {
   return strtok(input, " ");
 }
@@ -13,6 +15,23 @@ void echo(char *string) {
   if (string != NULL && *string != '\0') {
     printf("%s\n", string);
   }
+}
+
+void type(char *string){
+  const char *builtins[NUM_BUILTINS] = {
+    "echo",
+    "exit",
+    "type"
+  };  
+
+  for(int i=0; i<NUM_BUILTINS; i++){
+    if(strcmp(string, builtins[i])==0){
+      printf("%s is a shell builtin\n", string);
+      return;
+    }
+  }
+
+  printf("%s: not found\n", string);
 }
 
 int main(int argc, char *argv[]) {
@@ -43,6 +62,8 @@ int main(int argc, char *argv[]) {
     // echo command
     if (strcmp(command, "echo") == 0) {
       echo(command_arg);
+    } else if (strcmp(command, "type") == 0){
+      type(command_arg);
     } else {
       printf("%s: command not found\n", input);
     }
