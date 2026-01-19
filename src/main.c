dc38e0f7011b4c81a96beed9ef18f26f8cc4279a
ChethanJS07 <jschethan75@gmail.com>
2026-01-19 13:27:53 +0530

    added exit command

--- src/main.c
+++ src/main.c
@@ -11,6 +11,9 @@ int main(int argc, char *argv[]) {
     char command[1024];
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")]='\0';
+    if(strcmp(command, "exit")==0){
+      break;
+    }
     printf("%s: command not found\n", command);
   }
 
