ce6db63f56c7019625f40fd37bad95ebdabd2a9b
ChethanJS07 <jschethan75@gmail.com>
2026-01-19 21:44:43 +0530

    external process execution

--- src/main.c
+++ src/main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define NUM_BUILTINS 3
 
-char *getCommand(char *input) { return strtok(input, " "); }
-
-char *getCommandArg(char *input) { return strtok(NULL, "\0"); }
+void clear_screen() { printf("\033[2J\033[H"); }
 
 void echo(char *string) {
   if (string != NULL && *string != '\0') {
@@ -27,24 +27,24 @@ void type(char *string) {
   }
 
   char *path_env = getenv("PATH");
-  if(path_env==NULL){
+  if (path_env == NULL) {
     printf("%s: not found\n", string);
     return;
   }
 
   char path_copy[4096];
-  strncpy(path_copy, path_env, sizeof(path_copy)-1);
-  path_copy[sizeof(path_copy)-1]='\0';
+  strncpy(path_copy, path_env, sizeof(path_copy) - 1);
+  path_copy[sizeof(path_copy) - 1] = '\0';
 
   char *saveptr;
   char *dir = strtok_r(path_copy, ":", &saveptr);
 
-  while(dir!=NULL){
+  while (dir != NULL) {
     char full_path[PATH_MAX];
 
     snprintf(full_path, sizeof(full_path), "%s/%s", dir, string);
 
-    if(access(full_path, X_OK)==0){
+    if (access(full_path, X_OK) == 0) {
       printf("%s is %s\n", string, full_path);
       return;
     }
@@ -53,6 +53,37 @@ void type(char *string) {
   }
 
   printf("%s: not found\n", string);
+}
+
+void exec_external(char *command, char *args) {
+  char *argv[64];
+  int argc = 0;
+
+  argv[argc++] = command;
+
+  if (args != NULL) {
+    char *saveptr;
+    char *arg = strtok_r(args, " ", &saveptr);
+
+    while (arg != NULL && argc < 63) {
+      argv[argc++] = arg;
+      arg = strtok_r(NULL, " ", &saveptr);
+    }
+  }
+
+  argv[argc] = NULL;
+
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    execvp(command, argv);
+    perror(command);
+    exit(1);
+  } else if (pid > 0) {
+    waitpid(pid, NULL, 0);
+  } else {
+    perror("fork");
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -76,22 +107,34 @@ int main(int argc, char *argv[]) {
       break;
     }
 
+    if (strcmp(input, "clear") == 0) {
+      clear_screen();
+      continue;
+    }
+
     // get the command
-    char *command = getCommand(input);
-    char *command_arg = getCommandArg(input);
+    char *saveptr;
+    char *command = strtok_r(input, " ", &saveptr);
+    if (command == NULL) {
+      continue;
+    }
 
     // echo command
     if (strcmp(command, "echo") == 0) {
-      echo(command_arg);
+      char *args = saveptr;
+      echo(args);
     } else if (strcmp(command, "type") == 0) {
-      char *saveptr;
-      char *arg = strtok_r(command_arg, " ", &saveptr);
+      char *arg;
+      char *arg_saveptr;
+      arg = strtok_r(saveptr, " ", &arg_saveptr);
+
       while (arg != NULL) {
         type(arg);
-        arg = strtok_r(NULL, " ", &saveptr);
+        arg = strtok_r(NULL, " ", &arg_saveptr);
       }
+      continue;
     } else {
-      printf("%s: command not found\n", input);
+      exec_external(command, saveptr); 
     }
   }
 
